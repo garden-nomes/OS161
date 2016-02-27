bdee3656c79fb2da478c473fc079d1230016126b
noahw <noahbweiner@gmail.com>
2016-02-27 09:57:03 -0800

    finish sys__exit/sys_waitpid implementation

--- kern/syscall/proc_syscalls.c
+++ kern/syscall/proc_syscalls.c
@@ -11,18 +11,19 @@
 #include <copyinout.h>
 #include <mips/trapframe.h>
 
-  /* this implementation of sys__exit does not do anything with the exit code */
-  /* this needs to be fixed to get exit() and waitpid() working properly */
-
 void sys__exit(int exitcode) {
 
   struct addrspace *as;
   struct proc *p = curproc;
-  /* for now, just include this to keep the compiler from complaining about
-     an unused variable */
-  (void)exitcode;
 
   DEBUG(DB_SYSCALL,"Syscall: _exit(%d)\n",exitcode);
+
+  /* handle proc structure stuffs */
+  p->p_exited = true;
+  p->p_exit_code = exitcode;
+  lock_acquire(p->p_exit_lock);
+  cv_broadcast(p->p_exit_cv, p->p_exit_lock);
+  lock_release(p->p_exit_lock);
 
   KASSERT(curproc->p_addrspace != NULL);
   as_deactivate();
@@ -40,9 +41,26 @@ void sys__exit(int exitcode) {
   /* note: curproc cannot be used after this call */
   proc_remthread(curthread);
 
-  /* if this is the last user process in the system, proc_destroy()
-     will wake up the kernel menu thread */
-  proc_destroy(p);
+  if (p->pp_pid == 0) {	/* pid 0 is kernel process */
+	  /* destroy process */
+
+	  /* deal with children */
+	  for (int i = 1; i < MAX_PID; ++i) {
+		  if (proc_table[i] != NULL && proc_table[i]->pp_pid == p->p_pid) {
+				  if (proc_table[i]->p_exited) {
+					  /* destroy if exited */
+					  proc_destroy(proc_table[i]);
+				  } else {
+					  /* if not exited, adopt by grandparent */
+					  proc_table[i]->pp_pid = p->pp_pid;
+				  }
+		  }
+	  }
+
+	  /* if this is the last user process in the system, proc_destroy()
+		 will wake up the kernel menu thread */
+	  proc_destroy(p);
+  }
   
   thread_exit();
   /* thread_exit() does not return, so we should never get here */
@@ -72,6 +90,9 @@ int sys_fork(struct trapframe* tf, pid_t* retval) {
 		return result;
 	}
 
+	/* set process knows who made it */
+	child_proc->pp_pid = curproc->p_pid;
+
 	/* copy trapframe and modify it */
 	child_tf = kmalloc(sizeof(struct trapframe));
 	if (child_tf == NULL) {
@@ -92,8 +113,6 @@ int sys_fork(struct trapframe* tf, pid_t* retval) {
 	return 0;
 }
 
-
-/* stub handler for getpid() system call                */
 int
 sys_getpid(pid_t *retval)
 {
@@ -101,15 +120,12 @@ sys_getpid(pid_t *retval)
   return(0);
 }
 
-/* stub handler for waitpid() system call                */
-
 int
 sys_waitpid(pid_t pid,
 	    userptr_t status,
 	    int options,
 	    pid_t *retval)
 {
-  int exitstatus;
   int result;
 
   if (options != 0) {
@@ -122,16 +138,20 @@ sys_waitpid(pid_t pid,
 	return(ESRCH);
   }
 
+  if (proc_table[pid]->pp_pid != curproc->p_pid) {
+	/* not our child */
+	return(ECHILD);
+  }
+
   /* wait for exit */
-  if (!proc_table[pid]->exited) {
-	lock_aquire(proc_table[pid]->p_exit_lock);
-	cv_wait(proc_table[pid]->p_exit_cv);
+  if (!proc_table[pid]->p_exited) {
+	lock_acquire(proc_table[pid]->p_exit_lock);
+	cv_wait(proc_table[pid]->p_exit_cv, proc_table[pid]->p_exit_lock);
 	lock_release(proc_table[pid]->p_exit_lock);
   }
   
   /* return exit status */
-  exitstatus = proc_table[pid]->exit_code;
-  result = copyout((void *)&proc_table[pid]->exit_code, status, sizeof(int));
+  result = copyout((void *)&proc_table[pid]->p_exit_code, status, sizeof(int));
   if (result) {
     return(result);
   }
@@ -140,4 +160,3 @@ sys_waitpid(pid_t pid,
   *retval = pid;
   return(0);
 }
-
